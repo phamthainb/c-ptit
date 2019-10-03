10c198ad59290748b2568768fba1e1d1a1c86cda
phamthainb <phamthai4643@gmail.com>
2019-10-03 21:38:41 +0700

    update bai 6

--- lcm&gcd_6/main.cpp
+++ lcm&gcd_6/main.cpp
@@ -1,30 +1,34 @@
 // bai 6
 #include<iostream>
 #include<fstream>
+#include<algorithm>
 
 using namespace std;
 
 ifstream iff("inp.txt", ios::in);
 ofstream off("out.txt", ios::out);
 
-int _gcd(long long a, long long b){
-	while(a != b){
-		if(a > b) a = a - b;
-		else b = b - a;
-	}
-	return a;
-}
 int main(){
 	int times;
 	iff>>times;
 	while(times--){
-		int n,m, sum, subtract, a, b;
+		long long n, m, sum;
 		iff>>n>>m;
-		sum = n*(n+1)/2;
-		b = (n*n + n -2*m)/4;
-		a = sum - b;
-		if(_gcd(a, b) == 1) off<<"Yes"<<endl;
-		else off<<"No"<<endl;
-	}
+		sum = (n*n + n)/2;
+		if( m > sum ) off<<"No"<<endl;
+		else {
+			if( sum%2 == m%2 ){
+				long long x,y;
+				x = (sum + m)/2;
+				y = sum - x;
+				if(__gcd(x, y) == 1) off<<"Yes"<<endl;
+				else off<<"No"<<endl;
+			}
+			else off<<"No"<<endl; 
+		}
+	}	
 }
+
+
+
 
