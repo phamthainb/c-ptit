93f1b54dacdbf203c029fad0ef1ecf242a56a963
phamthainb <phamthai4643@gmail.com>
2019-10-29 00:16:46 +0700

    code

---
+++ modulo_6/main.cpp
@@ -0,0 +1,19 @@
+// bai 31 modulo_6
+#include<bits/stdc++.h>
+
+using namespace std;
+
+ifstream iff("inp.txt", ios::in);
+//ofstream off("out.txt", ios::out);
+
+int main(){
+	int times;
+	iff>>times;
+	while(times--){
+		string a;
+		long long b, m;
+		iff>>a>>b>>m;
+		
+	}
+	return 0;
+}
