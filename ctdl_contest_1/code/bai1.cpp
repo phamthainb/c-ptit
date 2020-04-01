e85baa7ec9d5a6f1d5a8c9294b9d079445680099
phamthainb <phamthai4643@gmail.com>
2020-04-01 07:17:29 +0700

    reset mind c++

---
+++ ctdl_contest_1/code/bai1.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int times;
+	cin>>times;
+	while(times--){
+		char str[1001];
+		// cout<<"input here"<<endl;
+		cin>>str;
+		// cout<<str<<endl;
+		for (int i = 0; i < strlen(str); ++i)
+		{
+			cout<<str[i]<<" ";
+		}
+		// cin.
+	}
+	
+	return 0;
+}
